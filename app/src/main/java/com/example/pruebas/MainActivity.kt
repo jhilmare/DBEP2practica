fd57ead2acd47e5e79086eceeb5b5c687e3658c1
Jhilmare <cyberjhilmar@gmail.com>
2026-06-09 16:41:28 -0500

    exam

--- app/src/main/java/com/example/pruebas/MainActivity.kt
+++ app/src/main/java/com/example/pruebas/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             PruebasTheme {
-                ComponentsGuideScreen()
+                FruitDetailScreen()
             }
         }
     }
